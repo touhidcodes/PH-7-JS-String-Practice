a99f63feabb2bc60401277665dbad01d4f0eb169
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-01 23:07:26 +0600

    case

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = "blackpink";
+let userAccount = "BlaCkPiNk";
+
+userAccount = userAccount + userName;
+
+console.log(userAccount.toLowerCase());
+console.log(userAccount.toUpperCase());
+
+console.log(userAccount.toLocaleLowerCase());
+console.log(userAccount.toLocaleUpperCase());
