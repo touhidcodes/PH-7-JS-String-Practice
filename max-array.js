ba9307d93eaf1cc363f0c0fb56e71ced586ddeb8
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-02 17:44:04 +0600

    max-array

---
+++ max-array.js
@@ -0,0 +1,15 @@
+function maxArray(array) {
+	let largest = array[0];
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
+		if (element > largest) {
+			largest = element;
+		}
+	}
+	return largest;
+}
+
+const array = [167, 190, 120, 165, 137, 265];
+
+const tallest = maxArray(array);
+console.log("Tallest is: ", tallest);
