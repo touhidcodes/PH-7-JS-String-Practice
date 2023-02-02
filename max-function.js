085b5518dbb3d857955c6a50ccdfe34212020961
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-02 17:16:47 +0600

    max-function

---
+++ max-function.js
@@ -0,0 +1,12 @@
+function maxNumber(a, b, c) {
+	if (a > b && a > c) {
+		return a;
+	} else if (b > a && b > c) {
+		return b;
+	} else {
+		return c;
+	}
+}
+
+const maxNum = maxNumber(23, 45, 56);
+console.log(maxNum);
