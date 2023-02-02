330ef6f5ce23e25fd460f231f19aa126da1710a5
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-02 14:55:13 +0600

    math

---
+++ math.js
@@ -0,0 +1,38 @@
+// Power
+const power = Math.pow(3, 5);
+console.log(power);
+
+// ------------------------------
+
+// Absolute
+const num1 = 25;
+const num2 = 45;
+const gap = Math.abs(num1 - num2);
+console.log(gap);
+
+// ---------------------------------
+
+// Round
+const number = 3.4556;
+const fullNumber = Math.round(number);
+console.log(fullNumber);
+
+const number2 = 3.556;
+const fullNumber2 = Math.round(number2);
+console.log(fullNumber2);
+
+// ------------------------------
+
+// Ceil and Floor
+const number4 = 4.56;
+const result1 = Math.ceil(number4);
+console.log(result1);
+const result2 = Math.floor(number4);
+console.log(result2);
+
+// ---------------------------
+// Random
+console.log(Math.random());
+
+const random = Math.ceil(Math.random() * 100);
+console.log(random);
