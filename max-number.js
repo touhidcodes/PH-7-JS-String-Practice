acd2502bdad53043acaa528d6c9a1ad5dee8fac9
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-02 17:12:11 +0600

    max

---
+++ max-number.js
@@ -0,0 +1,11 @@
+const jim = 84;
+const della = 99;
+const chinku = 77;
+
+if (jim > della && jim > chinku) {
+	console.log("Jim will get the Cake");
+} else if (della > jim && della > chinku) {
+	console.log("Della will get the Cake");
+} else {
+	console.log("Chin will get the Cake");
+}
---
+++ max.js
@@ -0,0 +1,6 @@
+const jim = 84;
+const della = 99;
+const chinku = 77;
+
+const maxNumber = Math.max(jim, della, chinku);
+console.log(maxNumber);
